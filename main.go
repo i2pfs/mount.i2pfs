9a51e15374bdae7c95f02732eadbfd8822917468
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-17 17:06:20 +0300

    Initial commit, pt. 3

--- main.go
+++ main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
+	"os"
+	"syscall"
+
 	"github.com/i2pfs/mount.i2pfs/config"
+	"github.com/pborman/getopt/v2"
+	"github.com/sevlyar/go-daemon"
 	"github.com/xaionaro-go/log"
 )
+
+func usage() {
+	getopt.Usage()
+	os.Exit(int(syscall.EINVAL))
+}
 
 func main() {
 	cfg := config.GetDefaults()
 	cfg.ReloadConfig()
 
-	err := doMount(cfg.SamUrl, cfg.PeersFilePath)
+	helpFlag := getopt.BoolLong("help", 'h', "display help")
+	foregroundFlag := getopt.BoolLong("foreground", 'f', "run in foreground")
+	getopt.Parse()
+
+	if *helpFlag {
+		usage()
+		return
+	}
+
+	args := getopt.Args()
+
+	mountpoint := args[0]
+
+	if *foregroundFlag {
+		err := doMount(cfg.SamUrl, cfg.PeersFilePath, mountpoint)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	ctx := &daemon.Context{}
+
+	d, err := ctx.Reborn()
 	if err != nil {
-		log.Panic(err)
+		log.Fatal("Unable to daemonize: ", err)
+	}
+	if d != nil {
+		return
+	}
+	defer ctx.Release()
+
+	log.Debug("Daemonized")
+	err = doMount(cfg.SamUrl, cfg.PeersFilePath, mountpoint)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
--- mount.go
+++ mount.go
@@ -6,12 +6,12 @@ import (
 	"github.com/i2pfs/mount.i2pfs/signals"
 )
 
-func doMount(samUrl, peersFilePath string) error {
+func doMount(samUrl, peersFilePath, mountpoint string) error {
 	err := setRLimitNoFile(consts.RLIMIT_NOFILE)
 	if err != nil {
 		return err
 	}
-	fuseServer := fuse.NewServer("/tmp/test")
+	fuseServer := fuse.NewServer(mountpoint)
 	signals.Init(fuseServer)
 	fuseServer.Serve()
 	return nil
